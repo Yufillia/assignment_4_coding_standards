2f7c56892c36656ffcd800a10c7ae1101c31afa0
Yufillia <yuufiii29@gmail.com>
2025-03-11 15:17:19 +0800

    added code where prompts user to input 9 numbers to subtract to first_num and prints the total

--- first_number_minus_9_oither_numbers.py
+++ first_number_minus_9_oither_numbers.py
@@ -2,3 +2,8 @@
 first_num = int(input("Enter the first number: "))
 total = first_num
 
+for _ in range(9):
+    num = int(input("Enter a number: "))
+    total -= num  # Subtract each entered number from total
+
+print("Result:", total)
