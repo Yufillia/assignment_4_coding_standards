34ede9f372cec9315583faf692bdac6700953e28
Yufillia <yuufiii29@gmail.com>
2025-03-11 16:26:25 +0800

    added some code that will print the numbers list every time you enter a number, so you can see what's stored.

--- identify_unique_or_duplicate_numbers_until_invalid_input.py
+++ identify_unique_or_duplicate_numbers_until_invalid_input.py
@@ -4,9 +4,9 @@ while True:
     try:
         num = int(input("Enter a number (or non-number to stop): "))
         if num in numbers:
-            print("Duplicate")
+            print("Duplicate", numbers)  # Show list for debugging
         else:
-            print("Unique")
+            print("Unique", numbers)  # Show list for debugging
             numbers.append(num)
     except ValueError:  # Stops when the user enters an invalid input
         break
