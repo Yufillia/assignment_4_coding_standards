05a9d264a140a5cdfae15e9c5b90ef5709e3ab10
Yufillia <yuufiii29@gmail.com>
2025-03-11 15:52:25 +0800

    added a code that finds unique numbers(numbers that doesn't repeat)

--- display_numbers_without_duplicates.py
+++ display_numbers_without_duplicates.py
@@ -6,3 +6,9 @@ print("Enter 10 numbers:")
 for _ in range(10):
     num = int(input())
     numbers.append(num)
+
+for num in numbers:
+    if numbers.count(num) == 1:  # If the number appears only once, it's unique
+        unique_numbers.append(num)
+
+print("Numbers without duplicates:", unique_numbers)
